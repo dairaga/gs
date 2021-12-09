745387256c3ef8ab45e1628238f37791e057c657
kigi <kigi.chang@gmail.com>
2021-12-09 16:21:14 +0800

    [chore] add license to header

--- either/either.go
+++ either/either.go
@@ -1,3 +1,8 @@
+// Copyright © 2022 Kigi Chang <kigi.chang@gmail.com>
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 package either
 
 import (
--- funcs/funcs.go
+++ funcs/funcs.go
@@ -1,3 +1,8 @@
+// Copyright © 2022 Kigi Chang <kigi.chang@gmail.com>
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 package funcs
 
 type Func[T, R any] func(T) R
