34039df8d5ae7fb07990b9af2cc73e70172c87f9
kigi <kigi.chang@gmail.com>
2021-12-10 17:59:06 +0800

    [docs] either

--- either/doc.go
+++ either/doc.go
@@ -1,4 +1,4 @@
 /*
-aaa
+Package either includes methods of Either, but implement them with functions because Go 1.18 does not support type parameters in methods.
 */
 package either
--- either/either.go
+++ either/either.go
@@ -10,20 +10,24 @@ import (
 	"github.com/dairaga/gs/funcs"
 )
 
-// TODO: refactor the method when go 1.19 releases.
+// TODO: refactor following functions to methods when go 1.19 releases.
 
+// Fold applies given function right to value from Right,
+// or function left to value From Left.
 func Fold[L, R, T any](e gs.Either[L, R],
 	left funcs.Func[L, T], right funcs.Func[R, T]) T {
 
 	return funcs.BuildUnit(e.Fetch, funcs.UnitAndThen(e.Left, left), right)
 }
 
+// FlapMap returns result applying given function op on value from Right, or returns Left.
 func FlatMap[L, R, T any](e gs.Either[L, R],
 	op funcs.Func[R, gs.Either[L, T]]) gs.Either[L, T] {
 
 	return Fold(e, gs.Left[L, T], op)
 }
 
+// Map returns new Right with result of applying function op if e is a Right, or return Left.
 func Map[L, R, T any](e gs.Either[L, R],
 	op funcs.Func[R, T]) gs.Either[L, T] {
 
