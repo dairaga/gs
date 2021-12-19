fe4cfa15ba611591ee25ab45d26132d57b334351
kigi <kigi.chang@gmail.com>
2021-12-19 11:51:07 +0800

    [docs] cherry pick from main

--- maps/doc.go
+++ maps/doc.go
@@ -1,4 +1,9 @@
+// Copyright © 2022 Kigi Chang <kigi.chang@gmail.com>
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 /*
-aaa
+Package maps includes methods of map but implements with functions.
 */
 package maps
--- slices/doc.go
+++ slices/doc.go
@@ -1,4 +1,9 @@
+// Copyright © 2022 Kigi Chang <kigi.chang@gmail.com>
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 /*
-aaa
+Package slices includes methods of slice but implements with functions.
 */
 package slices
