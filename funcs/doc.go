85b8f14bdfd46e96ba055f8c5c9c2711bc6727dc
kigi <kigi.chang@gmail.com>
2021-12-19 11:39:38 +0800

    [docs] slices, maps

--- funcs/doc.go
+++ funcs/doc.go
@@ -1,3 +1,8 @@
+// Copyright © 2022 Kigi Chang <kigi.chang@gmail.com>
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 /*
 Package funcs provides base function definition, composing and ordering.
 */
--- option/doc.go
+++ option/doc.go
@@ -1,3 +1,8 @@
+// Copyright © 2022 Kigi Chang <kigi.chang@gmail.com>
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 /*
 Package option includes methods of Option but implements them with functions.
 */
